2c72042f58e04a4fc2ef31eb5350bf2324eca4e0
Sam Macaluso <willsc8forwings@gmail.com>
2013-02-27 16:47:04 -0500

    Project setup
    Just setup the base files. nothing in it

---
+++ src/chootest/ChooTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chootest;
+
+/**
+ *
+ * @author phenomics
+ */
+public class ChooTest {
+    
+}
