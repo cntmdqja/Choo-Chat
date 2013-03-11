acabf82a903a1483a2e43fa2d05282143fb1d1eb
Sam Macaluso <willsc8forwings@gmail.com>
2013-03-11 16:08:16 -0400

    Added ChooChat.java (test)

---
+++ src/choochat/ChooChat.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package choochat;
+
+/**
+ *
+ * @author phenomics
+ */
+public class ChooChat {
+    
+}
